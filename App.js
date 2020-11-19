3b7217b45e9156de1cd98f1abf09020c98953327
Damian Martinez <damian.martinezd@softtek.com>
2020-11-18 23:01:23 -0600

    adding code

--- App.js
+++ App.js
@@ -8,42 +8,73 @@
 
 import React from 'react';
 import {
+  SafeAreaView,
   StyleSheet,
+  ScrollView,
   View,
-  Text
+  Text,
+  StatusBar,
 } from 'react-native';
 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-export default class MyAboutUsApp extends Component<Props> {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <View style={styles.body}>
-        <Text style={styles.sectionTitle}>About US</Text>
-        <Text>
-          The Electrode Native container is a native library similar to common third-party libraries that Android and 
-          iOS developers are familiar with. The container is generated entirely by the Electrode Native platform and 
-          packaged as an Android (AAR) library for Android and as a Framework for iOS.
-        </Text>
-    </View>
-    )
-  }
-}
-
-
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.sectionTitle}>About US</Text>
+      <Text>
+        The Electrode Native container is a native library similar to common third-party libraries that Android and 
+        iOS developers are familiar with. The container is generated entirely by the Electrode Native platform and 
+        packaged as an Android (AAR) library for Android and as a Framework for iOS.
+        Podria add more info
+      </Text>
+  </View>
+  )
+};
 
 const styles = StyleSheet.create({
-
-  body: {
-    backgroundColor: "#ccddee",
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color:"#DDFFE3",
-  }
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
+
+export default App;
